0f6b59112fbd01929778043a4b5247c29900c76c
explorer2005 <dhruvguptaofficial2005@gmail.com>
2024-09-21 09:42:13 +0530

    unique elements of the array

--- Lecture8/Q4.cpp
+++ Lecture8/Q4.cpp
@@ -5,16 +5,22 @@ int main(){
     int n;
     cin>>n;
     int arr[n];
+    
     cout<<"Enter the elements of the array"<<endl;
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
     cout<<"The unique elements are"<<endl;
     for(int i=0;i<n;i++){
+        int flag=1;
         for(int j=i+1;j<n;j++){
-            if(arr[i]!=arr[j]){
-                cout<<arr[i]<<endl;
+            if(arr[i]==arr[j]){
+                flag=0;
+                break;
             }
+        }
+        if(flag==1){
+            cout<<arr[i]<<" ";
         }
     }
     return 0;
